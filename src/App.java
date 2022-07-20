f9c66ae75c1304594cb6f20ab5ec89492d775778
DenerThiago21 <denerthi.ibi@gmail.com/>
2022-07-20 20:54:09 -0300

    terminando aula 3

--- src/App.java
+++ src/App.java
@@ -6,14 +6,19 @@ public class App {
     // link da api: https://api.mocki.io/v2/549a5d8b
     // link api2: https://api.mocki.io/v2/549a5d8b/NASA-APOD
     public static void main(String[] args) throws Exception {
-        // fazer uma conexão HTTP e buscar os top 250 filmes do imdb
-        String url = "https://api.mocki.io/v2/549a5d8b/NASA-APOD";
+        // api do imdb
+        String url = "https://api.mocki.io/v2/549a5d8b";
+        ExtratorConteudo extrator = new ExtratorConteudoImdb();
+
+        // api da nasa
+        // String url = "https://api.mocki.io/v2/549a5d8b/NASA-APOD";
+        // ExtratorConteudo extrator = new ExtratorConteudoNasa();
+
 
         ClienteHttp http = new ClienteHttp();
         String json = http.buscaDados(url);
 
         // exibir os dados extraídos
-        ExtratorConteudoNasa extrator = new ExtratorConteudoNasa();
         List<Conteudo> conteudos = extrator.extrairConteudo(json);
 
         GeradoraDeFigurinhas geradora = new GeradoraDeFigurinhas();
--- src/ExtratorConteudoNasa.java
+++ src/ExtratorConteudoNasa.java
@@ -2,7 +2,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 
-public class ExtratorConteudoNasa {
+public class ExtratorConteudoNasa implements ExtratorConteudo{
     
     public List<Conteudo> extrairConteudo (String json) {
 
